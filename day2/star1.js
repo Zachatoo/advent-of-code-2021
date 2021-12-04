50b75b70e6543be61f5f19339b6d4310f7c23f9f
Zach Young <zachatoo@gmail.com>
2021-12-04 12:56:52 -0700

    day2star1

---
+++ day2/star1.js
@@ -0,0 +1,34 @@
+import { data } from "./data.js";
+
+let pos = {
+  horizontal: 0,
+  depth: 0,
+};
+
+data.forEach(x => {
+  const direction = x.split(' ')[0];
+  const distance = +x.split(' ')[1];
+  switch (direction) {
+    case 'forward':
+      pos = {
+        ...pos,
+        horizontal: pos.horizontal + distance,
+      };
+      break;
+    case 'up':
+      pos = {
+        ...pos,
+        depth: pos.depth - distance,
+      }
+      break;
+    case 'down':
+      pos = {
+        ...pos,
+        depth: pos.depth + distance,
+      }
+      break;
+    default:
+      console.error(`unknown direction: ${direction}`);
+  }
+});
+console.log(pos.horizontal * pos.depth);
