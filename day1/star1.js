0f759a996e82938313760be32270a420b6f16287
Zach Young <zachatoo@gmail.com>
2021-12-04 09:27:05 -0700

    day1star1

---
+++ day1/star1.js
@@ -0,0 +1,9 @@
+import { data } from './star1data.js';
+
+let increaseCount = 0;
+data.forEach((x, i) => {
+  if (i !== 0) {
+    increaseCount += x > data[i - 1];
+  }
+});
+console.log(increaseCount);
