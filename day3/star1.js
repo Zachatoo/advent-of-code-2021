713326351bdbd013f8717c4a3593b1f68b1f079d
Zach Young <zachatoo@gmail.com>
2021-12-04 13:24:01 -0700

    day3star1

---
+++ day3/star1.js
@@ -0,0 +1,40 @@
+import { data } from './data.js';
+
+function getMinMaxKeyFromObjectByValue (obj) {
+  let min = Infinity;
+  let minKey = '';
+  let max = 0;
+  let maxKey = '';
+  for (let key in obj) {
+    if (obj[key] < min) {
+      min = obj[key];
+      minKey = key;
+    }
+    if (obj[key] > max) {
+      max = obj[key];
+      maxKey = key;
+    }
+  }
+  return [minKey, maxKey];
+}
+
+let gammaRateBits = '';
+let espilonRateBits = '';
+const maxBitIndex = data[0].length - 1;
+for (let i = 0; i <= maxBitIndex; ++i) {
+  const counts = {};
+  data.forEach(x => {
+    if (counts[x[i]] === undefined) {
+      counts[x[i]] = 1;
+    } else {
+      counts[x[i]] += 1;
+    }
+  });
+  const [ espilonRateBit, gammaRateBit ] = getMinMaxKeyFromObjectByValue(counts);
+  gammaRateBits += gammaRateBit;
+  espilonRateBits += espilonRateBit;
+}
+const gammaRateDecimal = parseInt(gammaRateBits, 2);
+const espilonRateDecimal = parseInt(espilonRateBits, 2);
+const powerConsumption = gammaRateDecimal * espilonRateDecimal;
+console.log(powerConsumption);
