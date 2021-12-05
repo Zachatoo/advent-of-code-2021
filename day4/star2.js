5ff88d6c81d60f3afd53ff314410125bd8905593
Zach Young <zachatoo@gmail.com>
2021-12-05 11:13:24 -0700

    day4star2

---
+++ day4/star2.js
@@ -0,0 +1,97 @@
+import { drawData, boardData } from './data.js';
+
+const MARK = 'x';
+
+function convertBoardDataToArrays(str) {
+  const boards = str.split(/\n\n/); // each board is separated by an empty newline
+  return boards.map((board) => {
+    const rows = board.split(/\n/).filter((x) => x.trim()); // each row in a board is separated by a newline
+    return rows.map((row) => {
+      const entries = row.split(/\W+/).map((x) => parseInt(x, 10)); // each entry in a row is separated by whitespace
+      return entries;
+    });
+  });
+}
+
+function isMarked(entry) {
+  return entry === MARK;
+}
+
+function markDrawnNumberOnBoard(board, number) {
+  return board.map((row) => {
+    return row.map((entry) => {
+      if (entry === number) {
+        return MARK;
+      }
+      return entry;
+    });
+  });
+}
+
+function checkForWin(board) {
+  return checkForRowWin(board) || checkForColWin(board);
+}
+
+function checkForRowWin(board) {
+  for (let row of board) {
+    if (row.every((entry) => isMarked(entry))) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkForColWin(board) {
+  for (let index in board) {
+    let column = [];
+    for (let row of board) {
+      column.push(row[index]);
+    }
+    if (column.every((entry) => isMarked(entry))) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function getScore(board, winningDrawnNumber) {
+  let score = 0;
+  const reducer = (prev, curr) => (isNaN(curr) ? prev : prev + curr);
+  board.forEach((row) => {
+    score = row.reduce(reducer, score);
+  });
+  score *= winningDrawnNumber;
+  return score;
+}
+
+let boardsArr = convertBoardDataToArrays(boardData);
+let losingBoard;
+let currentDrawIndex = -1;
+while (
+  boardsArr.length > 0 &&
+  !losingBoard &&
+  currentDrawIndex < drawData.length
+) {
+  currentDrawIndex++;
+  const drawnNumber = drawData[currentDrawIndex];
+  boardsArr = boardsArr.map((board) =>
+    markDrawnNumberOnBoard(board, drawnNumber)
+  );
+  let boardsToRemove = [];
+  boardsArr.forEach((board, i) => {
+    if (checkForWin(board)) {
+      boardsToRemove.push(i);
+    }
+  });
+  if (boardsToRemove.length > 0) {
+    if (boardsArr.length > 1) {
+      boardsArr = boardsArr.filter((_, i) => !boardsToRemove.includes(i));
+    } else {
+      losingBoard = boardsArr[0];
+    }
+  }
+}
+
+console.log(losingBoard);
+const losingDrawnNumber = drawData[currentDrawIndex];
+console.log(getScore(losingBoard, losingDrawnNumber));
